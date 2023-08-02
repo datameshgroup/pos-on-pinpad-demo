cde8f2fcb9af9d2023e1dd14129cba5d5f177417
Vanessa Agustin <vanessa_agustin@yahoo.com>
2023-08-02 11:56:53 +1000

    + Updated code to start demo app on boot complete

---
+++ app/src/main/java/au/com/dmg/terminalposdemo/BootUpReceiver.java
@@ -0,0 +1,17 @@
+package au.com.dmg.terminalposdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootUpReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+
+    }
+}
